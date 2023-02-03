a7c383300b312f90ee69da3ffded1cc58fd6e9c9
fajar cahyono <fajarcahyono112021@gmail.com>
2023-02-04 00:40:22 +0700

    add config for app.js routing and initial setup

--- README.md
+++ README.md
@@ -1,17 +1,12 @@
 # COOLANT MANAGEMENT SYSTEMS
 
 ## Running Program
+
 - yarn dev (DEVELOPMENT)
 - yarn build (PRODUCTIOM)
 
-## API's Documentation
-| No | Feature | Methode | Endpoint | Auth | Params | Response |
-|----|---------|---------|----------|------|--------|----------|
-|    |         |         |          |      |        |          |
-|    |         |         |          |      |        |          |
-|    |         |         |          |      |        |          |
-|    |         |         |          |      |        |          |
-|    |         |         |          |      |        |          |
-|    |         |         |          |      |        |          |
+## API Docs
+
+- Using Thunder Extension on VS CODE
 
 # CahWeb - Website Creator
--- app.js
+++ app.js
@@ -1,20 +1,38 @@
+require('dotenv').config()
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const cors = require('cors');
+
+var routerV1 = require('./routes/v1/index');
+
+const { database } = require('./config/database')
+
+database.connect()
+console.log('DB Connecttion:');
+console.log({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    ssl: false
+});
 
 var app = express();
+
+app.use(cors())
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+console.log(path.join(__dirname));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1', routerV1);
 
 module.exports = app;
