fdbe7bdeb1debe288c1a0739e6c69d89b384d20d
fajar cahyono <fajarcahyono112021@gmail.com>
2023-02-04 23:43:42 +0700

    refactor routing and add route controller for public group

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,14 @@
 const { lines } = require('./masterdata')
 const { register, login } = require('./auth/index')
+const { getPublicGroup } = require('./public/groupController')
 
 
 module.exports = {
     // AUTH
     register,
     login,
+    // PUBLIC
+    getPublicGroup,
 
     lines
 }
--- controllers/public/groupController.js
+++ controllers/public/groupController.js
@@ -1,2 +1,15 @@
 const table = 'tb_m_group'
 const { queryGET } = require('../../helpers/query')
+const response = require('../../helpers/response')
+
+module.exports = {
+    getPublicGroup: (req, res) => {
+        queryGET(table, null, ['id', 'group_nm', 'is_deleted'])
+            .then((result) => {
+                console.log(result);
+                response.success(res, 'Success to get PUBLIC GROUP', result)
+            }).catch((err) => {
+                console.log(err);
+            });
+    }
+}
