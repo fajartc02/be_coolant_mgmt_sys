909eb3fe2f2d055b639afcd7988364d2e54e4c67
fajar cahyono <fajarcahyono112021@gmail.com>
2023-02-19 18:08:55 +0700

    add table config

---
+++ config/table.js
@@ -0,0 +1,26 @@
+const table = {
+    tb_m_lines: 'tb_m_lines',
+    tb_m_machines: 'tb_m_machines',
+    tb_m_check_params: 'tb_m_check_params',
+    tb_m_checksheets: 'tb_m_checksheets',
+    tb_m_chemical: 'tb_m_chemical',
+    tb_m_group: 'tb_m_group',
+    tb_m_machine_chemical: 'tb_m_machine_chemical',
+    tb_m_maintenance: 'tb_m_maintenance',
+    tb_m_options: 'tb_m_options',
+    tb_m_options_ranged: 'tb_m_options_ranged',
+    tb_m_parameters: 'tb_m_parameters',
+    tb_m_periodic: 'tb_m_periodic',
+    tb_m_rules: 'tb_m_rules',
+    tb_m_users: 'tb_m_users',
+    tb_r_chemical_changes: 'tb_r_chemical_changes',
+    tb_r_cron_log: 'tb_r_cron_log',
+    tb_r_logs: 'tb_r_logs',
+    tb_r_mt_schedules: 'tb_r_mt_schedules',
+    tb_r_periodic_check: 'tb_r_periodic_check',
+    tb_r_tasks: 'tb_r_tasks',
+}
+
+
+
+module.exports = table
